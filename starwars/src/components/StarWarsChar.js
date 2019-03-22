beca75defe8eedf960b43da7b48ae9df13736f1a
chubbard022 <chubbard02@gmail.com>
2019-03-22 08:25:05 -0700

    data is flowing down through props successfully

---
+++ starwars/src/components/StarWarsChar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const StarWarsChar = props =>{
+    return(
+        <div>
+            <p>{props.starWarsCharacter.name}</p>
+        </div>
+    )
+}
+
+export default StarWarsChar;
