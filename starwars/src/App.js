29b166167e241afdb9dd8d3a81b6fe6352e8c5eb
chubbard022 <chubbard02@gmail.com>
2019-03-22 08:53:22 -0700

    star wars character cards styling added, not done with all styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -34,7 +34,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-       < StarWarsCharList starWarsChar={this.state.starwarsChars}/>
+          <div className="starWarsHolder">
+            < StarWarsCharList starWarsChar={this.state.starwarsChars}/>
+          </div>
       </div>
     );
   }
--- starwars/src/components/StarWarsChar.js
+++ starwars/src/components/StarWarsChar.js
@@ -3,8 +3,12 @@ import React from 'react';
 
 const StarWarsChar = props =>{
     return(
-        <div>
-            <p>{props.starWarsCharacter.name}</p>
+        <div className='starWarsCard'>
+            <p className='characterAttribute'>Name:<p>{props.starWarsCharacter.name}</p></p>
+            <p className='characterAttribute'>Gender:{props.starWarsCharacter.gender}</p>
+            <p className='characterAttribute'>Height:{props.starWarsCharacter.height}</p>
+            <p className='characterAttribute'>Mass:{props.starWarsCharacter.mass}</p>
+            <p className='characterAttribute'>Birth-Year:{props.starWarsCharacter.birth_year}</p>
         </div>
     )
 }
