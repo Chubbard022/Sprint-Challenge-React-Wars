c00f93d30d60edd2503ee0c3b970eedef83a3ced
chubbard022 <chubbard02@gmail.com>
2019-03-22 09:08:48 -0700

    star wars card content title styled

--- starwars/src/components/StarWarsChar.js
+++ starwars/src/components/StarWarsChar.js
@@ -4,7 +4,7 @@ import React from 'react';
 const StarWarsChar = props =>{
     return(
         <div className='starWarsCard'>
-            <p className='characterAttribute'>Name:<p>{props.starWarsCharacter.name}</p></p>
+            <h1 className='characterAttribute title'>Name:{props.starWarsCharacter.name}</h1>
             <p className='characterAttribute'>Gender:{props.starWarsCharacter.gender}</p>
             <p className='characterAttribute'>Height:{props.starWarsCharacter.height}</p>
             <p className='characterAttribute'>Mass:{props.starWarsCharacter.mass}</p>
